bdb1852ccbb4fda1b8407fd0f2352663a74a375f
wesh yo <wesh>
2015-09-12 21:41:36 +0200

    added completions

--- Main.jl
+++ Main.jl
@@ -1,8 +1,6 @@
 using Gtk
-
-include(Pkg.dir() *  "Gtk/src/GLib/GLib.jl")
-using .GLib
-using .GLib.MutableTypes
+using Winston
+import Base.REPLCompletions.completions
 
 pastcmd = [""];
 
@@ -10,6 +8,9 @@ win = @GtkWindow("")
 setproperty!(win, :width_request, 1200)
 setproperty!(win, :height_request, 800)
 fontsize = 11
+
+canvas = Gtk.@Canvas()
+setproperty!(canvas,:height_request, 500)
 
 entry = @GtkEntry()
 setproperty!(entry, :text, "x = rand(100,1)");
@@ -40,7 +41,7 @@ textview[:can_focus] = false
 textview[:vexpand] = true
 
 scwindow = @GtkScrolledWindow()
-setproperty!(scwindow,:height_request, 600)
+setproperty!(scwindow,:height_request, 100)
 push!(scwindow,textview)
 adj = getproperty(scwindow,:vadjustment, GtkAdjustment)
 
@@ -87,15 +88,50 @@ for m in eachmatch( r"[\w_]+\(",getproperty(srcbuffer,:text,String) )
   Gtk.apply_tag(srcbuffer, "functionnames", Gtk.GtkTextIter(srcbuffer,m.offset) , Gtk.GtkTextIter(srcbuffer,m.offset+length(m.match)-1) )
 end
 
-
 g = @GtkGrid()
 
-g[1,1] = scwindow    # Cartesian coordinates, g[x,y]
-g[1,2] = entry
-g[2,1:2] = ntbook
+rightPan = @GtkPaned(:v)
+mainPan = @GtkPaned(:h)
+rightBox = @GtkBox(:v)
+
+setproperty!(rightPan, :width_request, 600)
+
+push!(rightBox,scwindow)
+push!(rightBox,entry)
+
+rightPan[1] = canvas
+rightPan[2] = rightBox
+
+mainPan[1] = rightPan
+mainPan[2] = ntbook
+
+#g[1] = mainPan
+
+#g[1,1] = rightPan
+#g[2,1] = ntbook
 setproperty!(g, :column_homogeneous, true) # setproperty!(g,:homogeoneous,true) for gtk2
 setproperty!(g, :column_spacing, 15)  # introduce a 15-pixel gap between columns
-push!(win, g)
+push!(win, mainPan)
+
+## Winston things
+
+if !Winston.hasfig(Winston._display,1)
+  Winston.ghf()
+  Winston.addfig(Winston._display, 1, Winston.Figure(canvas,Winston._pwinston))
+else
+  Winston._display.figs[1] = Winston.Figure(canvas,Winston._pwinston)
+end
+
+
+## TERMINAL
+
+function my_println(xs)
+  insert!(buffer,string(xs))
+  nothing
+end
+
+import Base.println
+#println(xs...) = my_println(xs...)
 
 function on_return_terminal(widget::GtkEntry)
 
@@ -173,26 +209,91 @@ function entry_key_press_cb(widgetptr::Ptr, eventptr::Ptr, user_data)
     return convert(Cint,true)
   end
 
+  if event.keyval == Gtk.GdkKeySyms.Tab
+    cmd = getproperty(widget,:text,String)
+
+    (comp,dotpos) = completions(cmd, endof(cmd))
+    dotpos = dotpos.start
+    prefix = dotpos > 1 ? cmd[1:dotpos-1] : ""
+
+    if(length(comp)>1)
+      out = "\n"
+      for i=1:length(comp)
+        out = "$out $(comp[i]) \t\t"
+        if mod(i,6) == 0
+          out = out * "\n"
+        end
+      end
+      out = out * "\n"
+      insert!(buffer,out)
+      out = prefix * Base.LineEdit.common_prefix(comp)
+      setproperty!(widget,:text,out)
+      set_position(widget,endof(out))
+
+    elseif !isempty(comp)
+      out = prefix * comp[1]
+      setproperty!(widget,:text,out)
+      set_position(widget,endof(out))
+    end
+
+    return convert(Cint,true)
+  end
+
   return convert(Cint,false)
 end
 signal_connect(entry_key_press_cb, entry, "key-press-event", Cint, (Ptr{Gtk.GdkEvent},), false)
 
-text_iter_forward_line(it::Mutable{Gtk.GtkTextIter})  = ccall((:gtk_text_iter_forward_line,  Gtk.libgtk),Cint,(Ptr{Gtk.GtkTextIter},),it)
-text_iter_backward_line(it::Mutable{Gtk.GtkTextIter}) = ccall((:gtk_text_iter_backward_line, Gtk.libgtk),Cint,(Ptr{Gtk.GtkTextIter},),it)
+text_iter_forward_line(it::Gtk.GLib.MutableTypes.Mutable{Gtk.GtkTextIter})  = ccall((:gtk_text_iter_forward_line,  Gtk.libgtk),Cint,(Ptr{Gtk.GtkTextIter},),it)
+text_iter_backward_line(it::Gtk.GLib.MutableTypes.Mutable{Gtk.GtkTextIter}) = ccall((:gtk_text_iter_backward_line, Gtk.libgtk),Cint,(Ptr{Gtk.GtkTextIter},),it)
+text_iter_forward_to_line_end(it::Gtk.GLib.MutableTypes.Mutable{Gtk.GtkTextIter}) = ccall((:gtk_text_iter_forward_to_line_end, Gtk.libgtk),Cint,(Ptr{Gtk.GtkTextIter},),it)
+
+function set_position(editable::Gtk.Entry,position_)
+    ccall((:gtk_editable_set_position,Gtk.libgtk),Void,(Ptr{Gtk.GObject},Cint),editable,position_)
+    return editable
+end
 
 function editor_key_press_cb(widgetptr::Ptr, eventptr::Ptr, user_data)
   widget = convert(GtkTextView, widgetptr)
   event = convert(Gtk.GdkEvent, eventptr)
 
+  @show event
+
   if event.keyval == Gtk.GdkKeySyms.Return && Int(event.state) == 5 #ctrl+shift
 
-    itstart = Gtk.GtkTextIter(srcbuffer,getproperty(srcbuffer,:cursor_position,Int)-2) #select current line
-    itend = Gtk.GtkTextIter(srcbuffer,getproperty(srcbuffer,:cursor_position,Int)+2) #select current line
-    text_iter_backward_line(itstart)
-    #text_iter_forward_line(itend)
-    Gtk.apply_tag(srcbuffer, "function", itstart , itend )
+    itstart = Gtk.GtkTextIter(srcbuffer,getproperty(srcbuffer,:cursor_position,Int)) #select current line
+    itend = Gtk.GtkTextIter(srcbuffer,getproperty(srcbuffer,:cursor_position,Int)) #select current line
 
-    show(itstart)
+    itstart = Gtk.GLib.MutableTypes.mutable(itstart)
+    itend = Gtk.GLib.MutableTypes.mutable(itend)
+
+    text_iter_backward_line(itstart)
+    skip(itstart,1,:line)
+    text_iter_forward_to_line_end(itend)
+
+    #Gtk.apply_tag(srcbuffer, "function", itstart , itend )
+
+    txt = getproperty(srcbuffer,:text,String)
+    txt = txt[getproperty(itstart,:offset,Int):getproperty(itend,:offset,Int)]
+    setproperty!(entry,:text,txt)
+
+    keyevent = Gtk.GdkEventKey(Gtk.GdkEventType.KEY_PRESS, Gtk.gdk_window(entry), Int8(0), UInt32(0), UInt32(0), Gtk.GdkKeySyms.Return, UInt32(0), convert(Ptr{Uint8},C_NULL), UInt16(13), UInt8(0), uint32(0) )
+    signal_emit(entry, "key-press-event", Bool, keyevent)
+
+    #creating an event ain't easy
+    #event = Gtk.GdkEventKey(8,Ptr{Void} @0x0000000013dde300,0,0x02a26074,0x00000000,0x0000ff0d,1,Ptr{UInt8} @0x000000001262a8c0,0x000d,0x00,0x00000000)
+    #immutable GdkEventKey <: GdkEvent
+    #    event_type::GEnum
+    #    gdk_window::Ptr{Void}
+    #    send_event::Int8
+    #    time::Uint32
+    #    state::Uint32
+    #    keyval::Uint32
+    #    length::Int32
+    #    string::Ptr{Uint8}
+    #    hardware_keycode::Uint16
+    #    group::Uint8
+    #    flags::Uint32
+    #end
 
     return convert(Cint,true)
   end
