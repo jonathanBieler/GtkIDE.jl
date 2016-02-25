0cb729b4e70c2808ee384efc49992bca9f2a5d17
Luciano Lorenti <lucianolorenti@gmail.com>
2016-02-24 23:35:25 -0300

    Starting with the files tree context menu

--- src/SidePanels.jl
+++ src/SidePanels.jl
@@ -10,6 +10,8 @@ function files_tree_view(rownames)
 
     tv = @GtkTreeView(GtkTreeModel(list))
 
+
+
     cols = Array(GtkTreeViewColumn,0)
 
     r1 = @GtkCellRendererPixbuf()
@@ -73,24 +75,44 @@ function select_value(tree_view::GtkTreeView,list::GtkTreeStore,v)
 end
 
 #### FILES PANEL
-
-function open_file(treeview::GtkTreeView,list::GtkTreeStore)
-
-    v = selected(treeview, list)    
+function get_selected_path(treeview::GtkTreeView,list::GtkTreeStore)
+    v = selected(treeview, list)
     if v != nothing && length(v) == 3
-        isfile(v[3]) && open_in_new_tab(v[3])
-        
+        return v[3]
+    else
+        return nothing
+    end
+end
+function get_selected_file(treeview::GtkTreeView,list::GtkTreeStore)
+    path = get_selected_path(treeview,list)
+    if (isfile(path))
+        return path
+    else
+        return nothing
+    end
+end
+function open_file(treeview::GtkTreeView,list::GtkTreeStore)
+    file = get_selected_file(treeview,list)
+    if file != nothing
+        open_in_new_tab(file)
     end
 end
 
 function filespanel_treeview_clicked_cb(widgetptr::Ptr, eventptr::Ptr, user_data)
     treeview = convert(GtkTreeView, widgetptr)
     event = convert(Gtk.GdkEvent, eventptr)
-    list = user_data
+    (list,menu) = user_data
 
-    if event.event_type == Gtk.GdkEventType.DOUBLE_BUTTON_PRESS
-        open_file(treeview,list)
+    if event.button == 3
+
+        showall(menu)
+        popup(menu,event)
+    else
+        if event.event_type == Gtk.GdkEventType.DOUBLE_BUTTON_PRESS
+            open_file(treeview,list)
+        end
     end
+
     return PROPAGATE
 end
 
@@ -105,43 +127,78 @@ function filespanel_treeview_keypress_cb(widgetptr::Ptr, eventptr::Ptr, user_dat
 
     return PROPAGATE
 end
+function create_file_cb(ptr::Ptr, data)
+    (path,filename) = data
+    println(getproperty(filename, :text, String))
+    return nothing
+end
+function filespanel_newFileItem_activate_cb(widgetptr::Ptr,user_data)
+
+    (list,treeview)     = user_data
+
+    path = get_selected_path(treeview,list)
+    if (path!=nothing)
+        path = dirname(path)
+        b = Gtk.GtkBuilderLeaf(filename=joinpath(dirname(@__FILE__),"forms/forms.glade"))
+        w = GAccessor.object(b,"DialogCreateFile")
+        btn_create_file = GAccessor.object(b,"btnCreateFile")
+        te_filename = GAccessor.object(b,"filename")
+        signal_connect(create_file_cb,btn_create_file, "clicked",Void,(),false,(path,te_filename))
+        showall(w)
+    end
+    return nothing
+end
 
 type FilesPanel <: GtkScrolledWindow
 
     handle::Ptr{Gtk.GObject}
     list::GtkTreeStore
     tree_view::GtkTreeView
-
+    menu::GtkMenu
     function FilesPanel()
 
         (tv,list,cols) = files_tree_view(["Icon","Name"])
 
+        menu = @GtkMenu(file) |>
+            (newFileItem = @GtkMenuItem("New File")) |>
+            (newFolderItem = @GtkMenuItem("New Folder")) |>
+            @GtkSeparatorMenuItem() |>
+            (quitMenuItem = @GtkMenuItem("Quit"))
+
+
         signal_connect(filespanel_treeview_clicked_cb,tv, "button-press-event",
-        Cint, (Ptr{Gtk.GdkEvent},), false,list)
+        Cint, (Ptr{Gtk.GdkEvent},), false,(list,menu))
         signal_connect(filespanel_treeview_keypress_cb,tv, "key-press-event",
         Cint, (Ptr{Gtk.GdkEvent},), false,list)
 
         sc = @GtkScrolledWindow()
         push!(sc,tv)
 
-        t = new(sc.handle,list,tv)
+
+
+
+        signal_connect(filespanel_newFileItem_activate_cb, newFileItem,
+                        "activate", Void, (), false, (list,tv))
+
+
+        t = new(sc.handle,list,tv,menu)
         Gtk.gobject_move_ref(t,sc)
     end
 end
 
 function update!(w::FilesPanel, path::AbstractString, parent=nothing)
+    pixbuf = GtkIconThemeLoadIconForScale(GtkIconThemeGetDefault(),"folder",24,1,0)
+    folder = push!(w.list,(pixbuf,basename(path),path),parent)
     n = readdir(path)
     for el in n
         full_path = joinpath(path,string(el))
         if isdir(full_path)
-            pixbuf = GtkIconThemeLoadIconForScale(GtkIconThemeGetDefault(),"folder",24,1,0)
-            folder = push!(w.list,(pixbuf,el),parent)
             update!(w,full_path, folder )
         else
            file_parts = splitext(el)
            if  (file_parts[2]==".jl")
              pixbuf = GtkIconThemeLoadIconForScale(GtkIconThemeGetDefault(),"code",24,1,0)
-             push!(w.list,(pixbuf,el, joinpath(path,el)),parent)
+             push!(w.list,(pixbuf,el, joinpath(path,el)),folder)
            end
          end
     end
