241eff770f26384bf1d17e6abb7782c55b105da7
Jonathan Bieler <jonathan.bieler@alumni.epfl.ch>
2017-12-03 20:20:44 +0100

    working on markdown view

--- dev/test_markdown.jl
+++ dev/test_markdown.jl
@@ -14,7 +14,7 @@ Test *italic*
 
 """
 
-m = string(@doc rand)
+m = string(@doc collect)
 
 m = Base.Markdown.parse(m)
 
@@ -49,10 +49,37 @@ function insert_MD!(buffer,m::String,i)
     i += length(m)
 end
 
+function insert_MD!(buffer,m::Markdown.LaTeX,i)
+    i = insert_MD!(buffer,m.formula,i)
+end
+
+
+function insert_MD!(buffer,m::Markdown.Paragraph,i)
+#    insert!(buffer,"\n\n")
+#    i += 2
+    for el in m.content
+        i = insert_MD!(buffer,el,i)
+    end
+    i
+end
+
 function insert_MD!(buffer,m::Markdown.Code,i)
     insert!(buffer,m.code)
     tag(buffer, "code", i, i+sizeof(m.code)) 
     i += length(m.code)
+end
+
+function insert_MD!(buffer,m::Markdown.List,i)
+    for it in m.items
+        insert!(buffer,"    - ")
+        i += 6
+        for el in it
+            i = insert_MD!(buffer,el,i)
+        end
+        insert!(buffer,"\n")
+        i += 1
+    end 
+    i
 end
 
 function insert_MD!(buffer,m::Markdown.Italic,i)
@@ -89,27 +116,52 @@ function insert_MD!(buffer,m::Markdown.MD)
     end
 end
 
-#
+type MarkdownTextView <: GtkTextView
+
+    handle::Ptr{Gtk.GObject}
+    view::GtkTextView
+    buffer::GtkTextBuffer
+
+    function MarkdownTextView(txt::Markdown.MD)
+        
+        buffer = GtkTextBuffer()
+        
+        view = GtkTextView(buffer)
+
+        setproperty!(view,:margin_left,1)
+        setproperty!(view,:monospace,true)
+        setproperty!(view,:wrap_mode,true)
+
+        Gtk.create_tag(buffer, "normal", font="13")
+        Gtk.create_tag(buffer, "h1", font="Bold 15")
+        Gtk.create_tag(buffer, "h2", font="bold 14")
+        Gtk.create_tag(buffer, "bold", font="bold")
+        Gtk.create_tag(buffer, "italic", font="italic")
+        Gtk.create_tag(buffer, "code", font="bold", background="#eee")
+
+        insert_MD!(buffer,m)
+        tag(buffer,"normal",1,length(buffer))
+        
+        n = new(view.handle,view,buffer)
+        Gtk.gobject_move_ref(n, view)
+    end
+
+end
+
+##
+
+
 
 using Gtk
 import Gtk.GtkTextIter
 
-buffer = GtkTextBuffer()
+#buffer = GtkTextBuffer()
 #setproperty!(buffer,:text,"wesh")
 
-view = GtkTextView(buffer)
-setproperty!(view,:margin_left,10)
-setproperty!(view,:monospace,true)
+view = MarkdownTextView(m)
 
-Gtk.create_tag(buffer, "normal", font="13")
-Gtk.create_tag(buffer, "h1", font="Bold 15")
-Gtk.create_tag(buffer, "h2", font="bold 14")
-Gtk.create_tag(buffer, "bold", font="bold")
-Gtk.create_tag(buffer, "italic", font="italic")
-Gtk.create_tag(buffer, "code", font="normal", background="#eee")
-
-insert_MD!(buffer,m)
-tag(buffer,"normal",1,length(buffer))
-
-w = GtkWindow(view)
+w = GtkWindow("",600,400)
+push!(w,view)
 showall(w)
+
+
